0e67d3539fa291f3f26b3690ffb1c030a984771d
darthpasta <liowj.nick@gmail.com>
2017-06-25 03:30:18 +0800

    added strong params to users, added validations for name & username

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,6 @@ class Task < ApplicationRecord
 	belongs_to :user
 
 	validates :description, length: {minimum: 1}
+	validates :username, presence: true, length: {maximum: 15}
+	validates :name, presence: true, length: {maximum: 20}
 end
