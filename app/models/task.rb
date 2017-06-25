4bd139f21544eec3401bba793c0d2751ea601f74
darthpasta <liowj.nick@gmail.com>
2017-06-25 17:35:59 +0800

    Fixed errors in validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,5 @@
 class Task < ApplicationRecord
 	belongs_to :user
-
 	validates :description, length: {minimum: 1}
-	validates :username, presence: true, length: {maximum: 15}
-	validates :name, presence: true, length: {maximum: 20}
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,8 @@ ActiveRecord::Schema.define(version: 20170624182947) do
     t.boolean "completed?"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +43,5 @@ ActiveRecord::Schema.define(version: 20170624182947) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
