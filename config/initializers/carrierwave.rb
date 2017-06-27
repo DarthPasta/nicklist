b43b5bd7001cbc824e3dc442a9ef4515e437a68c
darthpasta <liowj.nick@gmail.com>
2017-06-27 22:47:59 +0800

    prepare for deploy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,5 @@
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
-
-  if Rails.env.test? || Rails.env.cucumber? || Rails.env.development?
-    config.storage = :file
-    config.enable_processing = false
-    config.root = '#{Rails.root}/tmp'
-  else
-    config.storage = :fog
-  end
-
-
 	config.fog_credentials = {
 		:provider => 'aws',
 		:aws_access_key_id => ENV['S3_KEY'],
@@ -17,6 +7,14 @@ CarrierWave.configure do |config|
 		:region => 'ap-southeast-1',
     :path_style => true
 	}
+
+    if Rails.env.test? || Rails.env.cucumber? || Rails.env.development?
+    config.storage = :file
+    config.enable_processing = false
+    config.root = '#{Rails.root}/tmp'
+  else
+    config.storage = :fog
+  end
 
   config.cache_dir = '#{Rails.root}/tmp/uploads'
   config.fog_directory = ENV['S3_BUCKET_NAME']
