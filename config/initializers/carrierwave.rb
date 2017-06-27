5ba64da7e370beeec3f90dcd00c87a2698af6157
darthpasta <liowj.nick@gmail.com>
2017-06-27 23:38:32 +0800

    fixed uploading, edit some margin

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,22 +1,24 @@
 require 'carrierwave/storage/fog'
-
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-	config.fog_credentials = {
-		:provider => 'aws',
-		:aws_access_key_id => ENV['S3_KEY'],
-		:aws_secret_access_key => ENV['S3_SECRET'],
-		:region => 'ap-southeast-1',
-    :path_style => true
-	}
 
-    if Rails.env.test? || Rails.env.cucumber? || Rails.env.development?
+   if Rails.env.test? || Rails.env.cucumber? || Rails.env.development?
     config.storage = :file
     config.enable_processing = false
     config.root = '#{Rails.root}/tmp'
   else
     config.storage = :fog
   end
+
+  config.fog_provider = 'fog/aws'
+	config.fog_credentials = {
+		:provider => 'aws',
+		:aws_access_key_id => ENV['S3_KEY'],
+		:aws_secret_access_key => ENV['S3_SECRET'],
+		:region => ENV['S3_REGION'],
+    :path_style => true
+	}
+
+ 
 
   config.cache_dir = '#{Rails.root}/tmp/uploads'
   config.fog_directory = ENV['S3_BUCKET_NAME']
