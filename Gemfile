3be1257ee8837f4763889fce4fb960a328f2a41e
darthpasta <liowj.nick@gmail.com>
2017-06-27 22:32:53 +0800

    fixed image display problem

--- Gemfile
+++ Gemfile
@@ -27,7 +27,7 @@ gem "figaro"
 # image upload
 gem 'carrierwave'
 gem 'mini_magick'
-gem 'fog'
+gem 'fog-aws'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,5 @@
 CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
 
   if Rails.env.test? || Rails.env.cucumber? || Rails.env.development?
     config.storage = :file
@@ -8,17 +9,19 @@ CarrierWave.configure do |config|
     config.storage = :fog
   end
 
+
 	config.fog_credentials = {
 		:provider => 'aws',
 		:aws_access_key_id => ENV['S3_KEY'],
 		:aws_secret_access_key => ENV['S3_SECRET'],
-		:region => 's3-us-east-2',
+		:region => 'ap-southeast-1',
     :path_style => true
 	}
 
   config.cache_dir = '#{Rails.root}/tmp/uploads'
   config.fog_directory = ENV['S3_BUCKET_NAME']
   config.fog_public = true
+
 
   # config.fog_host = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
   config.ignore_integrity_errors = false
