149acc66bce45fc0e2a055e8fa8790203ecf57cb
darthpasta <liowj.nick@gmail.com>
2017-06-29 23:56:15 +0800

    added AJAX to create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,20 +14,26 @@ before_action :authenticate_user!, :only => [:create]
 		@task.description = params[:task][:description]
 		@task.user_id = current_user.id 
 		@task.save
-		if @task.save
-			flash[:success] = "Successfully added a task"
-			redirect_to root_path
 
-		else
-			flash[:danger] = "Failed to create new task"
-			redirect_to root_path 
+		respond_to do |format|
+		format.html {redirect_to root_path}
+		format.js 
 		end
+
+		if @task.save
+
+		 	
+		
+
+		 else
+		 	flash[:danger] = "Failed to create new task"
+		 	redirect_to root_path 
+		 end
 	end 
 
 	def destroy
 		@task = Task.find(params[:id])
 		@task.destroy
-		debugger
 		if @task.destroy
 			flash[:success] = "Task deleted"
 			redirect_to root_path
